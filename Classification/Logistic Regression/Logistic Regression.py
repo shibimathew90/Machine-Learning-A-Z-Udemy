6509f7d323b8366c51065be3083f52ab58bfad69
shibimathew90 <45932718+shibimathew90@users.noreply.github.com>
2019-07-12 18:01:32 +0530

    Add files via upload

---
+++ Classification/Logistic Regression/Logistic Regression.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[4]:
+
+
+dataset = pd.read_csv('C:/Udemy/Machine-Learning-A-Z-New/Machine Learning A-Z New/Part 3 - Classification/Section 14 - Logistic Regression/Social_Network_Ads.csv')
+
+
+# In[5]:
+
+
+dataset.head()
+
+
+# In[8]:
+
+
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,-1].values
+
+
+# In[9]:
+
+
+X, y
+
+
+# In[10]:
+
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state= 0)
+
+
+# In[12]:
+
+
+X_train.shape, X_test.shape
+
+
+# In[13]:
+
+
+y_train.shape, y_test.shape
+
+
+# In[14]:
+
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+
+# In[15]:
+
+
+X_train.shape, X_test.shape
+
+
+# In[16]:
+
+
+X_train, X_test
+
+
+# In[17]:
+
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+# In[18]:
+
+
+y_pred = classifier.predict(X_test)
+
+
+# In[19]:
+
+
+y_pred
+
+
+# In[20]:
+
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_test, y_pred)
+
+
+# In[21]:
+
+
+cm
+
+
+# In[23]:
+
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy*100)
+
+
+# In[25]:
+
+
+#Visualizing Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step=0.01),
+                     np.arange(start=X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# In[26]:
+
+
+# Visualizing test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step=0.01),
+                     np.arange(start=X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
