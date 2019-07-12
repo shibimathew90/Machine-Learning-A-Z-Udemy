30fd2b17a12a4e1f05dbb31782f3433feafee124
shibimathew90 <45932718+shibimathew90@users.noreply.github.com>
2019-07-12 18:03:30 +0530

    Update Kernel SVM.py

--- Classification/Kernel SVM/Kernel SVM.py
+++ Classification/Kernel SVM/Kernel SVM.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 # In[2]:
 
 
-dataset = pd.read_csv('C:/Udemy/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 17 - Kernel SVM/Social_Network_Ads.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 
 
 # In[3]:
